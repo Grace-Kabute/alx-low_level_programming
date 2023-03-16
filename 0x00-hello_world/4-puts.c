7668bf5905d368440d568168565622d838113149
kabute muthoni <kabutemuthoni@gmail.com>
2023-03-16 16:21:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- *main-Entry point
+ * main - this is the main function
  *
- * *Return: Always 0(Success)
+ * Return:always 0
  */
+
 init main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
