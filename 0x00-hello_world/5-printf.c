2506675b596d36f31c8fed73381731d353d2c570
kabute muthoni <kabutemuthoni@gmail.com>
2023-03-16 17:12:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - ths is a function to print a line
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
