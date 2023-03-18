366bc56e6dff4322be6916b93384033ed9db04b7
kabute muthoni <kabutemuthoni@gmail.com>
2023-03-17 18:53:35 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - loop
+ * Return: always 0
+*/
+int main (void)
+{
+  int i;
+  for (i = 'a' ; i <= 'z' ; i++)
+    putchar(i);
+  putchar('\n');
+  return (0);
+}
