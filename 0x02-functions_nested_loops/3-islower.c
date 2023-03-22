5debb468fe719a95f56626562d9ab6d2213c7516
kabute muthoni <kabutemuthoni@gmail.com>
2023-03-21 19:19:15 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * -islower - print alphabets in lowercase
+ * 0c: parameter to be printed
+ * Return: 1 if lowercase otherwise 0
+*/
+int _islower(int c)
+{
+ if (c >= 97 && c <= 122)
+ return (1);
+ else
+ return (0);
+}
