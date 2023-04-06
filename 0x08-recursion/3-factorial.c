5898f3c881d17b075cfcc915dabb9be39eade24c
kabute muthoni <kabutemuthoni@gmail.com>
2023-04-05 22:30:36 -0700

    pow recursive

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+*factorial - gives factorial of an int
+*@n: input
+*Return: Always 0
+*/
+
+int factorial(int n)
+{
+if (n == 0)
+{
+return (1);
+}
+return (n * factorial(n - 1));
+}
