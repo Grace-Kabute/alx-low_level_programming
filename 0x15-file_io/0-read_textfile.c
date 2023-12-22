776ae5979262bd3ec3476a5fceb29f8bfdb352f1
kabute muthoni <kabutemuthoni@gmail.com>
2023-12-22 02:24:57 -0800

    file handling

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,27 +2,27 @@
 
 /**
  * create_file - Creates a file.
- * @filesname: A pointer to the name of the file to create.
- * @file_content: A pointer to a string to write to the file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
-int create_file(const char *filesname, char *file_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
-	if (filesname == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	if (file_content != NULL)
+	if (text_content != NULL)
 	{
-		for (len = 0; file_content[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
-	fd = open(filesname, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, file_content, len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
 		return (-1);
