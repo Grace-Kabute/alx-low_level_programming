cbf9b322cbdd590ada3647b79113384de0fb7339
kabute <kabutemuthoni@gmail.com>
2024-02-23 04:55:03 -0800

    hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,4 +1,13 @@
+#include "hash_tables.h"
 
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ *
+ * Return: The calculated hash.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
 	unsigned long int hash;
 	int c;
 
